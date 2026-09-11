81d8a110e1683bcaba5946fac29b6a1eeeedc00c
Amol Wadgaonkar <amol.wadgaonkar@acldigial.com>
2026-09-11 18:35:13 +0530

    change in authentication test- invalid creds

--- tests/authentication.spec.js
+++ tests/authentication.spec.js
@@ -12,7 +12,7 @@ test('TC02 - Verify user cannot login with invalid creds - wrong password', asyn
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
-    await expect(page.locator('[data-test="error"]')).toBeVisible();
+    await expect(page.getByRole('alert')).toBeVisible();
 });
 
 test('TC03 - Verify user cannot login with invalid creds - wrong username', async ({ page }) => {
@@ -20,7 +20,7 @@ test('TC03 - Verify user cannot login with invalid creds - wrong username', asyn
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
-    await expect(page.locator('[data-test="error"]')).toBeVisible();
+    await expect(page.getByRole('alert')).toBeVisible();
 });
 
 
